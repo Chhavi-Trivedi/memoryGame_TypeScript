c957df218be3e6b6a29f9380df64708e1ef636bf
Chhavi Trivedi <55316437+Chhavi-Trivedi@users.noreply.github.com>
2021-03-19 19:50:59 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import MemoryGame from './components';
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <p className='text'>A Memory game for TypeScript programmers </p>
+     <MemoryGame/>
+    </>
+  );
+}
+
+export default App;
