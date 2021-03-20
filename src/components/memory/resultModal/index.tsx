0b49b1706c169d96d01b094c37d062bc28c457d9
Chhavi Trivedi <55316437+Chhavi-Trivedi@users.noreply.github.com>
2021-03-20 22:27:48 +0530

    Update index.tsx

--- src/components/memory/resultModal/index.tsx
+++ src/components/memory/resultModal/index.tsx
@@ -156,6 +156,188 @@ interface PropTypes {
   renderItems: object
 }
 
+import * as React from 'react';
+import Modal from 'react-modal';
+
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)'
+  }
+};
+
+
+interface PropTypes {
+  isModalOpen: boolean,
+  onClose(event: React.MouseEvent<Element, MouseEvent>):void ,
+  title: string,
+  renderItems: object
+}
+
+
+const ModalComponent = ({
+  isModalOpen,
+  onClose,
+  title,
+  renderItems
+}: PropTypes) => {
+  return (
+    <div>
+      <Modal
+        isOpen={isModalOpen}
+        onRequestClose={onClose}
+        style={customStyles}
+        contentLabel={title}
+      >
+        {renderItems}
+      </Modal>
+    </div>
+  )
+}
+
+export default ModalComponent;
+
+/*
+import * as React from 'react';
+import Modal from 'react-modal';
+
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)'
+  }
+};
+
+
+interface PropTypes {
+  isModalOpen: boolean,
+  onClose(event: React.MouseEvent<Element, MouseEvent>):void ,
+  title: string,
+  renderItems: object
+}
+
+
+const ModalComponent = ({
+  isModalOpen,
+  onClose,
+  title,
+  renderItems
+}: PropTypes) => {
+  return (
+    <div>
+      <Modal
+        isOpen={isModalOpen}
+        onRequestClose={onClose}
+        style={customStyles}
+        contentLabel={title}
+      >
+        {renderItems}
+      </Modal>
+    </div>
+  )
+}
+
+export default ModalComponent;
+import * as React from 'react';
+import Modal from 'react-modal';
+
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)'
+  }
+};
+
+
+interface PropTypes {
+  isModalOpen: boolean,
+  onClose(event: React.MouseEvent<Element, MouseEvent>):void ,
+  title: string,
+  renderItems: object
+}
+
+
+const ModalComponent = ({
+  isModalOpen,
+  onClose,
+  title,
+  renderItems
+}: PropTypes) => {
+  return (
+    <div>
+      <Modal
+        isOpen={isModalOpen}
+        onRequestClose={onClose}
+        style={customStyles}
+        contentLabel={title}
+      >
+        {renderItems}
+      </Modal>
+    </div>
+  )
+}
+
+export default ModalComponent;
+
+/*
+import * as React from 'react';
+import Modal from 'react-modal';
+
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)'
+  }
+};
+
+
+interface PropTypes {
+  isModalOpen: boolean,
+  onClose(event: React.MouseEvent<Element, MouseEvent>):void ,
+  title: string,
+  renderItems: object
+}
+
+
+const ModalComponent = ({
+  isModalOpen,
+  onClose,
+  title,
+  renderItems
+}: PropTypes) => {
+  return (
+    <div>
+      <Modal
+        isOpen={isModalOpen}
+        onRequestClose={onClose}
+        style={customStyles}
+        contentLabel={title}
+      >
+        {renderItems}
+      </Modal>
+    </div>
+  )
+}
+
+export default ModalComponent;
+
+
 
 const ModalComponent = ({
   isModalOpen,
